31ac915f04e3c5ad73b442b67d9d7aa10e9edb69
MierafA12 <mierafabebe12@gmail.com>
2026-07-15 18:37:03 +0300

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { HeroSection } from "@/components/home/HeroSection";
 import { WhatWeDo } from "@/components/home/WhatWeDo";
-import { FeaturedProject } from "@/components/home/FeaturedProject";
 import { MissionVision } from "@/components/home/MissionVision";
 import { WhyChooseUs } from "@/components/home/WhyChooseUs";
 import { CTASection } from "@/components/home/CTASection";
@@ -10,7 +9,6 @@ export default function Home() {
     <>
       <HeroSection />
       <WhatWeDo />
-      <FeaturedProject />
       <MissionVision />
       <WhyChooseUs />
       <CTASection />
